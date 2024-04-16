44736c0a0f3f422d7b0fbe36e8c0e43b8efb1523
Fabián Macías Esparza <al294996@edu.uaa.mx>
2024-04-15 22:24:55 -0600

    Mesas

--- mesas.py
+++ mesas.py
@@ -1,8 +1,24 @@
 import tkinter as tk
 
+button_colors = ["green"] * 15
+buttons = []  # Define the "buttons" list
+
+def button_pressed(button_number):
+    if button_colors[button_number] == "green":
+        print(f"Button {button_number + 1} pressed")
+        button_colors[button_number] = "red"
+        buttons[button_number].config(bg="red")
+        buttons[button_number].config(fg="white")
+        buttons[button_number].config(state=tk.DISABLED)
+        # función para guardar la mesa en la base de datos
+        # guardar_mesa_en_base_de_datos(button_number + 1)
+        
 def mesas_ventana():
     nueva_ventana = tk.Toplevel()
     nueva_ventana.title("Mesas Disponibles")
     nueva_ventana.attributes('-fullscreen', True)
-    label = tk.Label(nueva_ventana, text="¡Hola desde la Ventana 3!")
-    label.pack()
+
+    for i in range(15):
+        button = tk.Button(nueva_ventana, text=f"Mesa {i + 1}", bg="green", width=15, height=5, font=("Helvetica", 14), command=lambda i=i: button_pressed(i))
+        button.grid(row=i // 5, column=i % 5, padx=80, pady=80)
+        buttons.append(button)
