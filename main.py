2e37d43f92dddfb35012376a9e2b492ed40d5268
JoseArmandoPerez <118926810+JoseArmandoPerez@users.noreply.github.com>
2024-05-04 20:32:59 -0600

    Front end conceta al servidor
    FABIAN REVIVE

--- main.py
+++ main.py
@@ -1,50 +1,19 @@
 import tkinter as tk
 from tkinter import messagebox
 from PIL import Image, ImageTk
-from recomendaciones import recomendaciones_ventana
-from acerca import acerca_ventana
-from mesas import mesas_ventana
-from pedidos_bebidas import abrir_ventana_pedidos
-from menu_comida import mostrar_menu
-from sesion import iniciar_sesion
+import sesion  # Importa el módulo de inicio de sesión
 import requests
-import json
-import time
 
-def check_server_status(url):
-    servidor_activo = False
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            print(response.text)
-            servidor_activo = True
-        else:
-            print(f"El servidor respondió con el código de estado: {response.status_code}")
-            servidor_activo = False
-    except requests.ConnectionError:
-        print("No se pudo conectar al servidor.")
-        servidor_activo = False
-    
-    return servidor_activo
-
-def main():
-    # Llamar a la función para mostrar el menú antes de iniciar la aplicación principal
-    # mostrar_menu()
-
-    app = RestauranteApp()
-    app.mainloop()
-
-class RestauranteApp(tk.Tk):  # Hereda de tk.Tk
+class RestauranteApp(tk.Tk):  
     def __init__(self):
         super().__init__()
         self.title("Interfaz con Botones")
-        self.attributes('-fullscreen', True)  # Set the window to fullscreen
+        self.attributes('-fullscreen', True)  
         
         self.nombre_usuario = None
         self.sesion_iniciada = False
         self.id = 0
 
-        # Cargar la imagen de fondo
         try:
             self.bg_image = Image.open("imagenes/background_image.png")
             self.bg_image = self.bg_image.resize((self.winfo_screenwidth(), self.winfo_screenheight()))
@@ -54,15 +23,14 @@ class RestauranteApp(tk.Tk):  # Hereda de tk.Tk
         except Exception as e:
             print("Error al cargar la imagen de fondo:", e)
 
-        # Nombre del restaurante
         restaurant_label = tk.Label(self, text="Ramen & Roll", font=("Helvetica", 36, "bold"), fg="white", bg="black")
         restaurant_label.place(relx=0.5, rely=0.1, anchor="center")
         
-        orders_button2 = tk.Button(self, text="Menu Comida", font=("Helvetica", 24), command=mostrar_menu, width=15)
-        orders_button2.place(relx=0.5, rely=0.3, anchor="center")
+        self.orders_button2 = tk.Button(self, text="Menu Comida", font=("Helvetica", 24), command=self.mostrar_menu, width=15)
+        self.orders_button2.place(relx=0.5, rely=0.3, anchor="center")
         
-        orders_button = tk.Button(self, text="Menu Bebidas", font=("Helvetica", 24), command=self.open_menu_bebidas, width=15)
-        orders_button.place(relx=0.5, rely=0.4, anchor="center")
+        self.orders_button = tk.Button(self, text="Menu Bebidas", font=("Helvetica", 24), command=self.open_menu_bebidas, width=15)
+        self.orders_button.place(relx=0.5, rely=0.4, anchor="center")
 
         about_button = tk.Button(self, text="Acerca de nosotros", font=("Helvetica", 24), command=self.open_about_window, width=15)
         about_button.place(relx=0.5, rely=0.5, anchor="center")
@@ -76,18 +44,15 @@ class RestauranteApp(tk.Tk):  # Hereda de tk.Tk
         close_button = tk.Button(self, text="Salir", font=("Helvetica", 24), command=self.close_all_windows, width=15)
         close_button.place(relx=0.5, rely=0.8, anchor="center")
         
-        # Botones de inicio y cierre de sesión
         self.sign_out_button = tk.Button(self, text="Cerrar sesión", font=("Helvetica", 24), command=self.sign_out, width=15)
         self.sign_out_button.place(relx=0.8, rely=0.1, anchor="center")
 
-        self.sign_in_button = tk.Button(self, text="Iniciar sesión", font=("Helvetica", 24), command=self.sign_in, width=15)
+        self.sign_in_button = tk.Button(self, text="Iniciar sesión", font=("Helvetica", 24), command=self.choose_login, width=15)
         self.sign_in_button.place(relx=0.8, rely=0.1, anchor="center")
         
-        # Llamar a la función para verificar si el servidor de http://192.168.100.89:5000/ está activo
-        servidor_esta_activo = check_server_status("http://100.72.9.175:5000")
+        servidor_esta_activo = check_server_status("http://100.82.213.30:5000")
         if servidor_esta_activo:
             print("El servidor está activo.")
-            # Mostrar un texto en la ventana principal
             restaurant_label = tk.Label(self, text="Servidor activo", font=("Helvetica", 12, "bold"), fg="green")
             restaurant_label.place(relx=0.1, rely=0.1, anchor="center")
         else:
@@ -106,7 +71,7 @@ class RestauranteApp(tk.Tk):  # Hereda de tk.Tk
         self.verificar_sesion()
         abrir_ventana_pedidos()
         
-    def open_menu_comida(self):
+    def mostrar_menu(self):
         self.verificar_sesion()
         mostrar_menu()    
             
@@ -115,18 +80,15 @@ class RestauranteApp(tk.Tk):  # Hereda de tk.Tk
         
     def sign_in(self):
         iniciar_sesion()
-        
+
     def sign_out(self):
-        guardar_estado_usuario(False, None, None)
         self.nombre_usuario = None
+        self.sesion_iniciada = False
         self.sign_in_button.place(relx=0.8, rely=0.1, anchor="center")
         self.sign_out_button.place_forget()
 
-    # Ver por que no se puede acceder al self
     def verificar_sesion(self):
-        self.sesion_iniciada, self.id, self.nombre_usuario = cargar_estado_usuario()
-        print(f"Estado del usuario: {self.sesion_iniciada}, {self.nombre_usuario}")
-        if(self.nombre_usuario != "None" or self.nombre_usuario != "null"):
+        if self.sesion_iniciada:
             self.sign_out_button.config(text=f"Salir como {self.nombre_usuario}")
             self.sign_in_button.place_forget()
             self.sign_out_button.place(relx=0.8, rely=0.1, anchor="center")
@@ -134,29 +96,90 @@ class RestauranteApp(tk.Tk):  # Hereda de tk.Tk
             self.sign_out_button.config(text="Iniciar sesión")
             self.sign_in_button.place(relx=0.8, rely=0.1, anchor="center")
             self.sign_out_button.place_forget()
-            
-def cargar_estado_usuario():
+
+    def choose_login(self):
+        login_window = tk.Toplevel(self)
+        login_window.title("Elige el tipo de sesión")
+        login_window.geometry("450x250")  # Ajusta el tamaño de la ventana
+
+        frame = tk.Frame(login_window, padx=20, pady=10)
+        frame.pack()
+        
+        tipo_usuario = tk.StringVar()
+        cliente_radio = tk.Radiobutton(frame, text="Cliente", variable=tipo_usuario, value="cliente")
+        cliente_radio.grid(row=0, column=0, sticky="w", pady=5)
+        chef_radio = tk.Radiobutton(frame, text="Chef", variable=tipo_usuario, value="chef")
+        chef_radio.grid(row=1, column=0, sticky="w", pady=5)
+        mesero_radio = tk.Radiobutton(frame, text="Mesero", variable=tipo_usuario, value="mesero")
+        mesero_radio.grid(row=2, column=0, sticky="w", pady=5)
+        barman_radio = tk.Radiobutton(frame, text="Bar-Man", variable=tipo_usuario, value="bar-man")
+        barman_radio.grid(row=3, column=0, sticky="w", pady=5)
+        duenio_radio = tk.Radiobutton(frame, text="Dueño", variable=tipo_usuario, value="dueño")
+        duenio_radio.grid(row=4, column=0, sticky="w", pady=5)
+        
+        button = tk.Button(frame, text="Ingresar", command=lambda: mostrar_formulario(tipo_usuario.get(), login_window))
+        button.grid(row=5, columnspan=2, pady=5, padx=5)
+
+def check_server_status(url):
+    servidor_activo = False
     try:
-        with open('config.json', 'r') as f:
-            data = json.load(f)
-            id_usuario = data['id_usuario']
-            nombre_usuario = data['nombre_usuario']
-            if(nombre_usuario != None or nombre_usuario != "null"):
-                print(f"El usuario {nombre_usuario} ha iniciado sesión.")
-                return True, id_usuario, nombre_usuario
-            else:
-                print("No hay usuario iniciado.")
-                return False, "error", "error"
-    except FileNotFoundError:
-        return False, "error", "error"
-
-
-def guardar_estado_usuario(usuario_iniciado, nombre_usuario, id_usuario):
-    with open('config.json', 'w') as f:
-        # Guarda el estado del usuario en un archivo JSON
-        json.dump({'usuario_iniciado': usuario_iniciado, 'nombre_usuario': nombre_usuario, 'id_usuario': id_usuario}, f)
-         
-if __name__ == "__main__":
-    guardar_estado_usuario(False, None, None)
+        response = requests.get(url)
+        if response.status_code == 200:
+            print(response.text)
+            servidor_activo = True
+        else:
+            print(f"El servidor respondió con el código de estado: {response.status_code}")
+            servidor_activo = False
+    except requests.ConnectionError:
+        print("No se pudo conectar al servidor.")
+        servidor_activo = False
     
-    main()
+    return servidor_activo
+
+def mostrar_formulario(tipo_usuario, login_window):
+    form_window = tk.Toplevel(login_window)
+    form_window.geometry("250x150")    
+    form_window.title("Inicio de Sesión")
+    
+
+    if tipo_usuario == "cliente":
+        name_label = tk.Label(form_window, text="Nombre:")
+        name_label.grid(row=0, column=0, sticky="w", pady=10)
+        name_entry = tk.Entry(form_window)
+        name_entry.grid(row=0, column=1, padx=10, pady=10)
+        
+        button = tk.Button(form_window, text="Ingresar", command=lambda: iniciar_sesion(name_entry.get(), tipo_usuario, form_window))
+        button.grid(row=1, columnspan=2, pady=10, padx=10)
+    else:
+        name_label = tk.Label(form_window, text="Nombre:")
+        name_label.grid(row=0, column=0, sticky="w", pady=10)
+        name_entry = tk.Entry(form_window)
+        name_entry.grid(row=0, column=1, padx=10, pady=10)
+
+        password_label = tk.Label(form_window, text="Contraseña:")
+        password_label.grid(row=1, column=0, sticky="w", pady=10)
+        password_entry = tk.Entry(form_window, show="*")
+        password_entry.grid(row=1, column=1, padx=10, pady=10)
+
+        button = tk.Button(form_window, text="Ingresar", command=lambda: iniciar_sesion(name_entry.get(), tipo_usuario, form_window))
+        button.grid(row=2, columnspan=2, pady=10, padx=10)
+
+def iniciar_sesion(nombre, tipo_usuario, window):
+    
+    # Si el tipo de usuario es "cliente", solo se necesita el nombre para iniciar sesión
+    if tipo_usuario == "cliente":
+        # Realiza la lógica de inicio de sesión para el cliente con el nombre proporcionado
+        # Aquí puedes llamar a la función de inicio de sesión del módulo "sesion.py"
+        sesion.iniciar_sesion_cliente(nombre)
+    else:
+        # Si el tipo de usuario no es "cliente", se requiere nombre y contraseña para iniciar sesión
+        # Puedes realizar la lógica de inicio de sesión con nombre y contraseña para otros tipos de usuarios
+        # Aquí puedes llamar a la función de inicio de sesión del módulo "sesion.py" con nombre y contraseña
+        sesion.iniciar_sesion_otro(nombre, password)
+        
+    # Cierra la ventana de inicio de sesión después de iniciar sesión
+    window.destroy()
+
+if __name__ == "__main__":
+    app = RestauranteApp()
+    app.mainloop()
--- sesion.py
+++ sesion.py
@@ -1,18 +1,23 @@
 import tkinter as tk
-import tkinter.messagebox as messagebox
+from tkinter import messagebox
 import requests
-import json
 
-# Define las variables de entrada como globales
-name_entry = None
-email_entry = None
-password_entry = None
-tipo_sesion = None
+def iniciar_sesion_cliente(nombre):
+    # Realiza la lógica de inicio de sesión para el cliente con el nombre proporcionado
+    data = {'nombre': nombre}
+    response = requests.post('http://100.82.213.30:5000/login', json=data)
 
-def guardar_estado_usuario(usuario_iniciado, nombre_usuario, id_usuario):
-    with open('config.json', 'w') as f:
-        # Guarda el estado del usuario en un archivo JSON
-        json.dump({'usuario_iniciado': usuario_iniciado, 'nombre_usuario': nombre_usuario, 'id_usuario': id_usuario}, f)
+    if response.status_code == 200:
+        # Si el inicio de sesión es exitoso, muestra un mensaje de éxito
+        messagebox.showinfo("Inicio de Sesión", "¡Inicio de sesión exitoso!")
+    else:
+        # Si el inicio de sesión falla, muestra un mensaje de error
+        messagebox.showerror("Inicio de Sesión", "Error al iniciar sesión.")
+
+def iniciar_sesion_otro(nombre, contraseña):
+    # Realiza la lógica de inicio de sesión para otros tipos de usuarios
+    # Puedes implementar la lógica de inicio de sesión con nombre y contraseña aquí
+    pass
 
 def iniciar_sesion():
     # Crea la ventana
@@ -20,106 +25,20 @@ def iniciar_sesion():
     window.title("Inicio de Sesión")
     sign_in(window)
 
-
 def sign_in(window):
     # Marco para contener los widgets
     frame = tk.Frame(window, padx=10, pady=10)
     frame.pack()
 
-    # Etiqueta y campo de entrada para el correo electrónico
-    email_label = tk.Label(frame, text="Email:")
-    email_label.grid(row=0, column=0, sticky="w", pady=5)
-    email_entry = tk.Entry(frame)
-    email_entry.grid(row=0, column=1, padx=5, pady=5)
-
-    # Etiqueta y campo de entrada para la contraseña
-    password_label = tk.Label(frame, text="Password:")
-    password_label.grid(row=1, column=0, sticky="w", pady=5)
-    password_entry = tk.Entry(frame, show="*")
-    password_entry.grid(row=1, column=1, padx=5, pady=5)
-
-    # Botón de inicio de sesión
-    button = tk.Button(frame, text="Iniciar sesión", command=lambda: validate_credentials(email_entry, password_entry, window))
-    button.grid(row=2, column=1, pady=5, padx=5)
-
-    # botón para crear una cuenta si no la tiene
-    create_account_button = tk.Button(frame, text="Crear cuenta", command=lambda: sign_up(window))
-    create_account_button.grid(row=3, column=1, pady=5, padx=5)
-
-
-def validate_credentials(email_entry, password_entry, window):
-    email = email_entry.get()
-    password = password_entry.get()
-    if email == "" or password == "":
-        messagebox.showerror("Sign In", "Please enter email and password.")
-        return
-    else:
-        data = {'email': email, 'password': password}
-        response = requests.post('http://192.168.100.89:5000/login', json=data)  # Define la variable `response` aquí
-    if response.status_code == 200:
-        # Obtenemos la información del usuario
-        datos_usuario = response.json()
-        id_usuario = datos_usuario["id"]
-        nombre = datos_usuario["nombre"]
-        tipo_sesion = datos_usuario["tipo_sesion"]
-
-        # Guardamos el estado del usuario
-        guardar_estado_usuario(True, nombre, id_usuario)
-
-        # Mostramos el mensaje de bienvenida
-        messagebox.showinfo("Sign In", f"Bienvenido, {nombre}! Tipo de sesión: {tipo_sesion}")
-        window.after(2000, window.destroy)
-        return
-    else:
-        # Mostramos un mensaje de error
-        messagebox.showerror("Error", "Correo electrónico o contraseña incorrectos.")
-        window.after(2000, window.destroy)
-
-def sign_up(window):
-    global name_entry, email_entry, password_entry  # Declara las variables globales
-    
-    window.destroy()
-    window = tk.Tk()  # Crea una nueva ventana
-    window.title("Create Account")
-    
-    # Marco para contener los widgets
-    frame = tk.Frame(window, padx=20, pady=10)
-    frame.pack()
-    
-    # Etiqueta y campo de entrada para el nombre
-    name_label = tk.Label(frame, text="Name:")
+    # Etiqueta y campo de entrada para el nombre de usuario
+    name_label = tk.Label(frame, text="Nombre:")
     name_label.grid(row=0, column=0, sticky="w", pady=5)
     name_entry = tk.Entry(frame)
     name_entry.grid(row=0, column=1, padx=5, pady=5)
-    
-    # Etiqueta y campo de entrada para el correo electrónico
-    email_label = tk.Label(frame, text="Email:")
-    email_label.grid(row=1, column=0, sticky="w", pady=5)
-    email_entry = tk.Entry(frame)
-    email_entry.grid(row=1, column=1, padx=5, pady=5)
-    
-    # Etiqueta y campo de entrada para la contraseña
-    password_label = tk.Label(frame, text="Password:")
-    password_label.grid(row=2, column=0, sticky="w", pady=5)
-    password_entry = tk.Entry(frame, show="*")
-    password_entry.grid(row=2, column=1, padx=5, pady=5)
-    
-    button_create = tk.Button(frame, text="Create Account", command=lambda: create_account(window))
-    button_create.grid(row=3, column=1, pady=5, padx=5)
 
-def create_account(window):
-    # Accede a las variables globales
-    global name_entry, email_entry, password_entry  
-    name = name_entry.get()
-    email = email_entry.get()
-    password = password_entry.get()
+    # Botón de inicio de sesión
+    button = tk.Button(frame, text="Iniciar sesión", command=lambda: iniciar_sesion_cliente(name_entry.get()))
+    button.grid(row=1, columnspan=2, pady=5, padx=5)
 
-    data = {'nombre': name, 'email': email, 'password': password}
-    response = requests.post('http://192.168.100.89:5000/crear_usuario', json=data)
-    if response.status_code == 201:
-        messagebox.showinfo("Create Account", response.json()['message'])
-    else:
-        messagebox.showerror("Create Account", "Failed to create account.")
-    
-    # Cerrar la ventana (o realizar cualquier otra acción necesaria)
-    window.destroy()
+if __name__ == "__main__":
+    iniciar_sesion()
